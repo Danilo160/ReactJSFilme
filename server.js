c5b01e51f927fdea96f00a67ebe96a48c9d70528
Danilo160 <danilocorreiaviana@hotmail.com>
2022-04-03 17:45:53 -0300

    Atualização ReactJS

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use('/src', express.static(__dirname+'/src'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/src/App.js');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server online');
+});
