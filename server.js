10d3fcaefae43579803b4f6543b0f9dfe7b484b5
Danilo160 <danilocorreiaviana@hotmail.com>
2022-04-03 18:25:38 -0300

    Config express server

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const cors = require('cors');
+
+const { resolve } = require('path');
 
 const app = express();
-app.use(cors());
-app.use('/src', express.static(__dirname+'/src'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/src/App.js');
-});
+app.use('/', 
+  express.static(
+    resolve(
+      __dirname,
+      './build'
+  )
+ )
+)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server online');
