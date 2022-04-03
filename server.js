8583f8a2ded3f423324807c5395f1fe2bdfed8f8
Danilo160 <danilocorreiaviana@hotmail.com>
2022-04-03 20:00:04 -0300

    Atualização

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
-app.use('/src', express.static(__dirname+'/src/'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname+'./build');
-});
+app.use('/', express.static('./build'))
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server online');
