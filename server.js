0b16ed637a1986dd68e3f665a412a35c47baa28a
Danilo160 <danilocorreiaviana@hotmail.com>
2022-04-03 21:33:27 -0300

    TESTE

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use('/', express.static(__dirname+'./build'));
+app.use('/', express.static('./build'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+'./build');
+    res.sendFile('./build');
 });
 
 app.listen(process.env.PORT || 3000, () => {
