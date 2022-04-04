abf13451922ade534d42059c2b036ca3e546c5dd
Danilo160 <danilocorreiaviana@hotmail.com>
2022-04-03 21:04:47 -0300

    Att server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const { resolve } = require('path')
 const app = express();
 
-app.use('/', express.static('./build'))
+app.use('/', express.static(resolve(__dirname, './build'))
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server online');
